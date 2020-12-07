42bce8ef7ed36344837462b4652bbec144552067
PennyAna <law07006@byui.edu>
2020-12-07 16:03:56 -0700

    passport added

--- server.js
+++ server.js
@@ -1,38 +1,97 @@
 const express = require('express');
 const path = require('path');
-var app = express();
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, '/')));
-app.set('port', (process.env.PORT || 5000));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const db = require('./db');
 
-app.get('/', function (req, res) { res.render('pages/index.ejs');})
-app.get('/postage', function (req, res) {
-    res.render('pages/postage.ejs');
- 
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(function(username, password, cb) { db.useres.findByUsername(username, function (err, user) {
+    if (err) {return cb(err);}
+    if (!user) {return cb(null, false);}
+    if (user.password != password) {return cb(null, false);}
+    return cb(null, user);
 });
-app.get('/db', async function(req, res) {
-    try {
-        const client = await pool.connect();
-        const tableOne = await client.query('SELECT * FROM test_table');
-        const tableTwo = await client.query('SELECT * FROM media_table');
-        const tableThree = await client.query('SELECT * FROM login_table');
-        const tableFour = await client.query('SELECT * FROM genre_table');
-        const results = { 
-            'tableOne': (tableOne) ? tableOne.rows: null,
-            'tableTwo': (tableTwo) ? tableTwo.rows: null,
-            'tableThree': (tableThree) ? tableThree.rows: null, 
-            'tableFour': (tableFour) ? tableFour.rows: null
-        };
-        res.render('pages/db.ejs', results);
-        client.release();
-    } catch (err) {
+}));
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
+passport.deserializeUser(function(id, cb) {
+    db.users.findById(id, function(err, user) {
+        if(err) {return cb(err);}
+        cb(null, user);
+});
+});
+// Create a new Express application.
+const app = express();
+app.set(express.static(path.join(__dirname, '/views')));
+app.set(express.static(path.join(__dirname, '/public')));
+app.set(express.static(path.join(__dirname, '/')));
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({extended:true }));
+app.use(require('express-session')({secret: 'keyboard cat', resave: false, saveUninitialized: false}));
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+// Define routes.
+app.get('/', 
+        function(req, res) {
+            res.render('index.ejs', {user: req.user});});
+app.get('/login', 
+        function(req, res) {
+            res.render('login.ejs');});
+app.post('/login', 
+        passport.authenticate('local', {failureRedirect: '/'}),
+        function(req, res) { 
+        res.redirect('/');});
+app.get('/logout', 
+        function(req, res) {
+            req.logout();
+            res.redirect('/');});
+app.get('/profile', 
+        require('connect-ensure-login').ensureLoggedIn(), 
+        function(req, res) {
+            res.render('profile', {user: req.user});});
+app.get('/main', 
+        function (req, res) { 
+            res.render('pages/index.ejs', {user:req.user});})
+app.get('/db', 
+        async function(req, res) {
+        try {
+            const client = await pool.connect();
+            const tableOne = await client.query('SELECT * FROM test_table');
+            const tableTwo = await client.query('SELECT * FROM media_table');
+            const tableThree = await client.query('SELECT * FROM login_table');
+            const tableFour = await client.query('SELECT * FROM genre_table');
+            const results = { 
+                'tableOne': (tableOne) ? tableOne.rows: null,
+                'tableTwo': (tableTwo) ? tableTwo.rows: null,
+                'tableThree': (tableThree) ? tableThree.rows: null, 
+                'tableFour': (tableFour) ? tableFour.rows: null
+            };
+            res.render('pages/db.ejs', results);
+            client.release();
+        } catch (err) {
         console.error(err);
         res.send("Error " + err);
-    }
-})
+        }})
 const {Pool} = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, 
