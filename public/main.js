3ab6bbdc88f3d70f39be731997e4b13726e61163
PennyAna <law07006@byui.edu>
2020-12-12 22:47:35 -0700

    fixing js

--- public/main.js
+++ public/main.js
@@ -1,14 +1,15 @@
 const login = document.getElementById('loginDiv');
-const profile = document.getElementById('profileDiv');document.getElementById('loginBtn').addEventListener('click', changeLoginView);
-document.getElementById('profileBtn').addEventListener('click', changeProfileView);
+// const profile = document.getElementById('profileDiv');
+document.getElementById('loginBtn').addEventListener('click', changeLoginView);
+// document.getElementById('profileBtn').addEventListener('click', changeProfileView);
 
 function changeLoginView() {
     if (login.className == 'hidden') {
         login.className = 'visible';
     }
 }
-function changeProfileView() {
-    if (profile.className == 'hidden') {
-        profile.className = 'visible';
-    }
-}          
+// function changeProfileView() {
+//     if (profile.className == 'hidden') {
+//         profile.className = 'visible';
+//     }
+// }          
