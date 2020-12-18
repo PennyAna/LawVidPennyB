eb8489c73fe7467a4dd3725f227a4643f35810e3
PennyAna <law07006@byui.edu>
2020-12-17 21:03:25 -0700

    still working on db

--- server.js
+++ server.js
@@ -216,11 +216,11 @@ console.log('Now listening for connections on port: ', app.get('port'));
 function runQuery (queryString, cb) {
   const results = {};
   const client = pool.connect();
-  client.query(queryString, (err, res) => {
+  client.query(queryString), (err, res) => {
     if (err) { 
       console.log(err.stack);
     }else {
       results = res.rows[0];
-  }})
+  }};
   client.release();
 };
